3351908c7a78eb156162a468b5d977c6b716ee0f
Mayank Raj Gupta <mayankrajgupta3112@gmail.com>
2025-04-23 23:34:50 +0530

    captain register route done

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const cookieParser = require('cookie-parser');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
@@ -18,6 +19,10 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
+
+
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+
 
 module.exports = app
